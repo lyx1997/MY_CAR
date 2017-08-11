f62046785ad65e4c889c916419cc4852ea831ecd
Mr.lyx <30894135+lyx1997@users.noreply.github.com>
2017-08-11 13:45:20 +0800

    Update Sensor_Module.c

--- Sensor_Module.c
+++ Sensor_Module.c
@@ -2,53 +2,81 @@
 
 void Sensor_Module()
 {
-	if(led1 == 0 && led2 != 0)
+	if(led1 == 1 && led2 == 1 && led3 == 1 && led4 == 0 && led5 == 0)
 	{
 		delay(5);
-		if(led1 == 0 && led2 != 0){
+		if(led1 == 1 && led2 == 1 && led3 == 1 && led4 == 0 && led5 == 0){
 		direction = left40;
 		}
 	}
-	if(led2 == 0 && led1 != 0)
+	if(led1 == 1 && led2 == 1 && led3 == 0 && led4 == 0 && led5 == 0)
 	{
 		delay(5);
-		if(led2 == 0 && led1 != 0){
-		direction = left10;
+		if(led1 == 1 && led2 == 1 && led3 == 0 && led4 == 0 && led5 == 0){
+		direction = left30;
 		}
 	}
-	if(led3 == 0)
+	if(led1 == 1 && led2 == 0 && led3 == 0 && led4 == 0 && led5 == 0)
 	{
 		delay(5);
-		if(led3 == 0){
-		direction = straight;
-		}
-	}
-	if(led4 == 0 && led5 != 0)
-	{
-		delay(5);
-		if(led4 == 0 && led5 != 0){
-		direction = right10;
-		}
-	}
-	if(led5 == 0 && led4 != 0)
-	{
-		delay(5);
-		if(led5 == 0 && led4 != 0){
-		direction = right40;
-		}
-	}
-	if((led2==0)&&(led1==0))
-	{
-		delay(5);
-		if((led2==0)&&(led1==0)){
+		if(led1 == 1 && led2 == 0 && led3 == 0 && led4 == 0 && led5 == 0){
 		direction = left20;
 		}
 	}
-	if((led4==0)&&(led5==0))
+	if(led1 == 0 && led2 == 1 && led3 == 0 && led4 == 0 && led5 == 0)
 	{
 		delay(5);
-		if((led4==0)&&(led5==0)){
+		if(led1 == 0 && led2 == 1 && led3 == 0 && led4 == 0 && led5 == 0){
+		direction = left10;
+		}
+	}
+	if(led1 == 0 && led2 == 1 && led3 == 1 && led4 == 0 && led5 == 0)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 1 && led3 == 1 && led4 == 0 && led5 == 0){
+		direction = left5;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 0 && led5 == 0)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 0 && led5 == 0){
+		direction = straight;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 1 && led5 == 0)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 1 && led5 == 0){
+			direction = right5;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 1 && led5 == 0)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 1 && led5 == 0){
+			direction = right10;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 0 && led5 == 1)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 0 && led5 == 1){
 			direction = right20;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 1 && led5 == 1)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 0 && led4 == 1 && led5 == 1){
+			direction = right30;
+		}
+	}
+	if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 1 && led5 == 1)
+	{
+		delay(5);
+		if(led1 == 0 && led2 == 0 && led3 == 1 && led4 == 1 && led5 == 1){
+			direction = right40;
 		}
 	}
 }
