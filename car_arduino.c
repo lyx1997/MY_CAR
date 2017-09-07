e32d06fe60396b19bfbcde6cced08e58205785b0
Mr.lyx <30894135+lyx1997@users.noreply.github.com>
2017-09-07 16:36:31 +0800

    Add files via upload

--- car_arduino.c
+++ car_arduino.c
@@ -7,10 +7,12 @@ Servo myservo;
 /* the smart car status structure */
 struct car_status {
 /* basic status */
-  int left; /* left motor pwm */
-  int right;  /* right motor pwm */
-  int steering; /* steering angle */
-  unsigned int sensor;  /* optic sensor value */
+	int left; /* left motor pwm */
+	int right;  /* right motor pwm */
+	int steering; /* steering angle */
+	int laststeering; /* last steering angle */
+	unsigned int sensor;  /* optic sensor value */
+
 };
 
 #define STRAIGHT        (0)
@@ -46,7 +48,7 @@ const unsigned int sensor_pins[NUM_OF_SENSOR_PINS] = {14,15,16,17,18};
  *    ,  255: 100% volatage for motor
  *  right: see left above
  */
-int motor_dual(int left, int right)
+void motor_dual(int left, int right)
 {
   if (left > 255) {
     left = 255;
@@ -106,15 +108,17 @@ unsigned int sensor(void)
  *  [-40, 40]
  *  tell the servo to go to the position
  */
-int steering(int angle)
+int steering(int angle,int lastangle)
 {
   if (angle > 40) {
     angle = 40;
   } else if (angle < -40) {
     angle = -40;
+  } else if (angle == 0){
+  	angle = lastangle;
   }
 
-  angle += SEETRING_OFFSET;
+    angle += SEETRING_OFFSET;
     myservo.write(angle);
 
     return angle;
@@ -127,33 +131,33 @@ int sensor2steering_array[33] = {
   RIGHT_15,				// 0x02
   RIGHT_30,				// 0x03
   STRAIGHT,				// 0x04
-  -5,					// 0x05
+  0,					// 0x05
   RIGHT_20,				// 0x06
   RIGHT_40,				// 0x07
   LEFT_15,				// 0x08
-  10,					// 0x09	
+  0,					// 0x09	
   STRAIGHT,				// 0x10
-  -7,					// 0x11
+  0,					// 0x11
   LEFT_20,				// 0xC
-  -6,					// 0xD
+  0,					// 0xD
   STRAIGHT,				// 0xE
-  -14,					// 0xF
+  0,					// 0xF
   LEFT_30,				// 0x10
   STRAIGHT,				// 0x11
-  7,					// 0x12
-  -5,					// 0x13
-  5,					// 0x14
+  0,					// 0x12
+  0,					// 0x13
+  0,					// 0x14
   STRAIGHT,				// 0x15
-  6,					// 0x16
-  -5,					// 0x17
+  0,					// 0x16
+  0,					// 0x17
   LEFT_30,				// 0x18
-  7,					// 0x19
-  7,					// 0x1A
+  0,					// 0x19
+  0,					// 0x1A
   STRAIGHT,				// 0x1B
   LEFT_40,				// 0x1C
-  8,					// 0x1D
-  9,					// 0x1E
-  NULL,					// 0x1F
+  0,					// 0x1D
+  0,					// 0x1E
+  1						// 0x1F
 }; 
 
 
@@ -168,9 +172,21 @@ int sensor2steering(unsigned int sensor)
 
 int steering2motor(int steering)
 {
-  car.left = 127;
-  car.right = 127;
-  motor_dual(car.left, car.right);
+	
+	if(steering >= 30){
+		car.left -= 30;
+	} else if(steering <= -30){
+		car.right -= 30;
+	} else if(steering >= 20 && steering < 30){
+		car.left -= 10;
+	} else if(steering <=-20 && steering > -30){
+		car.right -= 10;
+	} else{
+		car.left = 127;
+  		car.right = 127;
+	}
+	
+  	motor_dual(car.left, car.right);
 }
 
 void setup() 
@@ -201,7 +217,8 @@ void loop()
    //steering2motor(steering(sensor2steering(sensor_read())));
   car.sensor = sensor();
   car.steering = sensor2steering(car.sensor);
-  steering(car.steering);
+  steering(car.steering,car.laststeering);
+  car.laststeering = car.steering;
   steering2motor(car.steering);
 #if DEBUG
   Serial.print(car.left);
